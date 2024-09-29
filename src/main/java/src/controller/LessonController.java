86237ee2b78223dbe0825f347602482dd3815a07
Nguyen Anh <nguyennam4c7@gmail.com>
2024-09-29 23:02:07 +0700

    add file

--- src/main/java/src/controller/LessonController.java
+++ src/main/java/src/controller/LessonController.java
@@ -3,17 +3,38 @@ package src.controller;
 import org.springframework.web.bind.annotation.*;
 import src.constant.UrlConstant;
 import src.dto.reqest.LessonReq;
+import src.dto.response.LessonRes;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class LessonController {
 
     @PostMapping(UrlConstant.LESSON_ADD)
-    public Object addLesson(@RequestBody LessonReq req) {
+    public Object addLesson(@RequestBody List<LessonReq> req) {
+        if(req == null) req = new ArrayList<LessonReq>();
         return req;
     }
 
     @PutMapping(UrlConstant.LESSON_UPDATE)
     public Object updateLesson(@PathVariable("lesson_id") int id) {
-        
+        return id;
+    }
+
+    @DeleteMapping(UrlConstant.LESSON_DELETE)
+    public Object deleteLesson(@PathVariable("lesson_id") int id, @RequestBody LessonReq req) {
+        LessonRes res = new LessonRes();
+
+        res.setName(req.getName());
+        res.setType(req.getType());
+        res.setDescription(req.getDescription());
+        res.setOrder(req.getOrder());
+        res.setUrl(req.getUrl());
+        res.setCreatedDate(req.getCreatedDate());
+        res.setUpdatedDate(req.getUpdatedDate());
+        res.setStatus("INACTIVE");
+
+        return res;
     }
 }
