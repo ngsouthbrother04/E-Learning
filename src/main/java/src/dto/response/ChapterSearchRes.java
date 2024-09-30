15407ef3c3f61ca27f9f825ede2660765407a925
Nguyen Anh <nguyennam4c7@gmail.com>
2024-09-30 22:19:45 +0700

    add file

--- src/main/java/src/dto/response/ChapterSearchRes.java
+++ src/main/java/src/dto/response/ChapterSearchRes.java
@@ -1,4 +1,4 @@
-package src.dto.reqest;
+package src.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -14,5 +14,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Accessors(chain = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class ChapterSearchReq {
+public class ChapterSearchRes {
+
 }
